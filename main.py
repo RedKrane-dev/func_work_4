66e215408f1151b70c3563c64b48ab0046d2a3da
RedKrane86 <redkrane86@gmail.com>
2026-02-03 16:06:19 +0500

    formating and fixes

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 def max_number(a, b):
     if a >= b:
         return a
-    elif a < b:
+    else:
         return b
-
-print(max_number(10, 2), max_number(5, 8), max_number(3, 3))
 
 # 2
 def empty_function():
@@ -16,13 +14,19 @@ def even_numbers(n):
     for number in range(2, n + 1, 2):
         yield number
 
-for num in even_numbers(11):
-    print(num)
-
 # 4
 def test_max_number():
     assert max_number(10, 2), 'Ошибка для кейса a >= b '
     assert max_number(2, 10), 'Ошибка для кейса a <= b '
     assert max_number(10, 10), 'Ошибка для кейса a == b '
 
+
+# 1
+print(max_number(10, 2), max_number(5, 8), max_number(3, 3))
+
+# 3
+for num in even_numbers(11):
+    print(num)
+
+# 4
 test_max_number()
